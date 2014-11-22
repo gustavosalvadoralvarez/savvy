62365c675261d9eaef8d8c886fe6bf9d20d5e971
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2014-11-22 01:04:30 -0500

    prototype up

---
+++ ex/server.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var fs = require('fs')
+
+var PORT = process.argv[2]
+
+var httpServer = http.createServer(function server_example(req, res) {
+	var url = req.url;
+	if (RegExp('^/[^/]*$').test(url)) {
+		var variant = ~~Math.random() * 2 > 1 ? 'A' : 'B';
+		res.setHeader("Content-Type", 'text/html');
+		res.statusCode = 200;
+		fs.createReadStream('./static/pages/sample' + variant + '.html').pipe(res)
+	} else if (RegExp(/^\/static\/(.*)/).test(url)) {
+		var loc = '.' + url,
+			content_type;
+		switch (url.split('/static/')[1].split('/')[0]) { // e.g pages in /static/pages/foo/bar.html => 
+			case 'css':
+				content_type = "text/css";
+				break; // [/static/, /pages/foo/bar.html] => [pages, foo, bar.html]
+			case 'js':
+				content_type = "text/javascript";
+				break;
+			case 'img':
+				content_type = "image/" + path.extname(url);
+				break;
+			case 'pages':
+				content_type = "text/html";
+				break;
+			default:
+				content_type = "text/plain";
+		}
+		res.setHeader("Content-Type", content_type);
+		fs.stat(loc, function send_static(err, stat) {
+			if (err) return console.log(err);
+			if (stat.isFile()) {
+				res.statusCode = 200;
+				fs.createReadStream(loc).pipe(res)
+			} else {
+				res.statusCode = 404;
+				res.end("404 NOT FOUND")
+			}
+		})
+	}
+}).listen(PORT, function(err) {
+	if (err) {
+		console.log(err);
+	}
+	console.log('server listening on port ' + PORT)
+});
+
+var savvy = require('../index.js')(httpServer);
+
+var lab = savvy.lab;
+
+lab.observations.readStream(process.stdout);
--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var measurement = require('./lib/measurement.js');
-var lab = require('./lib/lab.js');
+var net = require('./lib/net.js');
 
-module.exports = function Savvy(state){
-	var self = this, _lab, net;
-	self.lab = _lab = lab(measurement(state));
+module.exports = Savvy;
 
-	self.install = function install(server){
-		self.net = net = require('./lib/net')(server, self.lab);
+function Savvy(server) {
+	var self = this,
+		_lab, _net;
+	self.lab = _lab = require('./lib/lab.js')(measurement());
+	(function install(server) {
+		self.net = _net = net(server, _lab);
 		return self;
-	}
+	})(server);
 	return self
 }
+/*
+var PORT = process.argv[2];
+if (PORT) {
+	var httpServer = require('http').createServer(function(req, res) {}).listen(PORT, function(err) {
+		if (err) {
+			console.log(err);
+		}
+		console.log('server listening on port ' + PORT)
+	});
+	var savvy = Savvy(httpServer);
+	var lab = savvy.lab;
+	var header_experiment = lab.poisson('header');
+}
+*/
